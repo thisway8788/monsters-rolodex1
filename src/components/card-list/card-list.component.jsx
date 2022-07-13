530fa53d2802141fe13d41ce83faae1a14630433
Jeung Hak Ham <motherfatherilu@gmail.com>
2022-07-12 23:26:22 -0400

    changed to functional component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,29 +1,12 @@
-import { Component } from 'react';
+import Card from '../card/card.component';
 import './card-list.styles.css';
-import './card.styles.css';
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-
-    return (
-      <div className='card-list'>
-        {monsters.map((monster) => {
-          const { name, email, id } = monster;
-          return (
-            <div className='card-container' key={id}>
-              <img
-                src={`https://robohash.org/${id}?set=set21&size=180x180`}
-                alt={`monster ${name}`}
-              />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => (
+  <div className='card-list'>
+    {monsters.map((monster) => {
+      return <Card key={monster.id} monster={monster} />;
+    })}
+  </div>
+);
 
 export default CardList;
